62150b9abdd7e548259c5711472dfc82f53af041
elshad.sharbatov <elshad.sharbatov@cybernet.az>
2021-07-16 12:55:07 +0400

    entity

--- src/main/java/com/haydikodlayalim/accountservice/api/AccountApi.java
+++ src/main/java/com/haydikodlayalim/accountservice/api/AccountApi.java
@@ -1,4 +1,29 @@
 package com.haydikodlayalim.accountservice.api;
 
+import com.haydikodlayalim.accountservice.entity.Account;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountApi {
+
+    public ResponseEntity<Account> get(String id) {
+
+    }
+
+    public ResponseEntity<Account> save(Account account) {
+
+    }
+
+    public ResponseEntity<Account> update(Account account) {
+
+    }
+
+    public void delete(String id) {
+
+    }
+
+    public ResponseEntity<Account> pagination() {
+
+    }
 }
---
+++ src/main/java/com/haydikodlayalim/accountservice/service/AccountService.java
@@ -0,0 +1,27 @@
+package com.haydikodlayalim.accountservice.service;
+
+import com.haydikodlayalim.accountservice.entity.Account;
+import org.springframework.http.ResponseEntity;
+
+public class AccountService {
+    public  Account get(String id) {
+
+    }
+
+    public  Account save(Account account) {
+
+    }
+
+    public  Account  update(Account account) {
+
+    }
+
+    public void delete(String id) {
+
+    }
+
+    public  Account  pagination() {
+
+    }
+
+}
