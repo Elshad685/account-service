806b3e06f26fbd6c190689144b1a207317842212
elshad.sharbatov <elshad.sharbatov@cybernet.az>
2021-07-16 11:31:36 +0400

    entity

--- src/main/java/com/haydikodlayalim/accountservice/entity/Account.java
+++ src/main/java/com/haydikodlayalim/accountservice/entity/Account.java
@@ -1,4 +1,44 @@
 package com.haydikodlayalim.accountservice.entity;
 
 public class Account {
+  private  String id;
+    private  String username;
+    private  String email;
+    private  String passwd;
+
+    public String getId() {
+        return id;
+    }
+
+    public Account setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Account setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Account setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public Account setPasswd(String passwd) {
+        this.passwd = passwd;
+        return this;
+    }
 }
