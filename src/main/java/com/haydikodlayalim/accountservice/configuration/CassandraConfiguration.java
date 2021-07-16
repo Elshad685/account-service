eec3cf420b636f958b635464f78dacb490b5ba4d
elshad.sharbatov <elshad.sharbatov@cybernet.az>
2021-07-17 03:49:45 +0400

    entity

--- src/main/java/com/haydikodlayalim/accountservice/configuration/CassandraConfiguration.java
+++ src/main/java/com/haydikodlayalim/accountservice/configuration/CassandraConfiguration.java
@@ -1,16 +1,12 @@
 package com.haydikodlayalim.accountservice.configuration;
 
-import com.datastax.oss.driver.api.core.CqlSession;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.cassandra.config.*;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
-import org.springframework.data.cassandra.*;
-
 @Configuration
-@EnableCassandraRepositories(basePackages = "com.baeldung.spring.data.cassandra.repository")
+@EnableCassandraRepositories
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
     @Value("${spcloud.cassandra.keyspace.name}")
     private String keyspaceName;
